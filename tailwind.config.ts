65893bb9005d89916c86d837ef5b2b700c88abef
Miguel Alino <miggyalino35@gmail.com>
2024-02-05 01:12:38 +0800

    config edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,10 @@ const config: Config = {
       borderRadius: {
         '5xl': '40px',
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      }
     },
   },
   plugins: [],
